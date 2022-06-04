977ca2be0d242d17422a6bf148fb7a153147b4de
KRILLEKS <igorosadchev@gmail.com>
2022-06-04 17:40:34 +0500

    Pathfinding
    pathfinding algorithm is done

--- Assets/Scripts/Pathfinding.cs
+++ Assets/Scripts/Pathfinding.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using Unity.Collections;
 using Unity.Mathematics;
 using UnityEngine;
+using System.Linq;
 
-// I'll use DOTS to make pathfinding (using A* pathfinding algorithm) cause it'll have better performance
-// Although I can make it without DOTS
+// I wanted to use DOTS to make pathfinding (using A* pathfinding algorithm) cause it'll have better performance
+// but I saw that ECS is forbidden although my solution won't be too different
+// I'll use list/array instead of Native list/array and I won't use jobs
 public class Pathfinding : MonoBehaviour
 {
    private struct PathNode
@@ -27,44 +29,118 @@ public class Pathfinding : MonoBehaviour
       {
          fCost = gCost + hCost;
       }
+
+      public int2 Position()
+      {
+         return new int2(x, y);
+      }
    }
+
+   private readonly int2[] neighbourOffsetsArray =
+   {
+      new (1, 0), // right
+      new (-1, 0), // left 
+      new (0, 1), // up
+      new (0, -1), // down
+      new (1, 1), // right up
+      new (-1, 1), // left up
+      new (1, -1), // right down
+      new (-1, -1), // left down
+   };
 
    private void FindPath(int2 startPosition, int2 endPosition)
    {
-      NativeArray<PathNode> pathNodeArray = new NativeArray<PathNode>(Constants.GRID_SIZE.x * Constants.GRID_SIZE.y, Allocator.Temp);
+      PathNode[] pathNodeArray = new PathNode[Constants.GRID_SIZE.x * Constants.GRID_SIZE.y];
 
-      // initialize array
-      for (int x = 0; x < Constants.GRID_SIZE.x; x++)
-         for (int y = 0; y < Constants.GRID_SIZE.y; y++)
-         {
-            PathNode pathNode = new PathNode();
-            pathNode.x = x;
-            pathNode.y = y;
-            pathNode.index = CalculateIndex(x, y, Constants.GRID_SIZE.x);
+      InitializeArray();
 
-            // TODO: gCost looks strange for me
-            pathNode.gCost = int.MaxValue; // we will change this so value doesn't really matter
-            pathNode.hCost = CalculateDistanceCost(new int2(x, y), endPosition);
-            pathNode.CalculateFCost();
-
-            pathNode.isWalkable = true;
-            pathNode.cameFromNodeIndex = -1; // we'll use -1 as invalid value
-
-            pathNodeArray[pathNode.index] = pathNode;
-         }
-
-      PathNode startNode = pathNodeArray[CalculateIndex(startPosition.x, startPosition.y, Constants.GRID_SIZE.x)];
+      PathNode startNode = pathNodeArray[CalculateIndex(startPosition.x, startPosition.y)];
       startNode.gCost = 0;
       startNode.CalculateFCost();
       pathNodeArray[startNode.index] = startNode;
-      
-      
-      
-      pathNodeArray.Dispose();
 
-      int CalculateIndex(int x, int y, int gridWidth)
+      int endNodeIndex = CalculateIndex(endPosition.x, endPosition.y);
+      PathNode endNode = pathNodeArray[endNodeIndex];
+
+      List<int> openList = new List<int>();
+      List<int> closedList = new List<int>();
+
+      openList.Add(startNode.index);
+
+      // pathfinding itself
+      while (openList.Count > 0)
       {
-         return x + y * gridWidth;
+         int currentNodeIndex = GetLowestFCostNodeIndex(openList, pathNodeArray);
+         PathNode currentNode = pathNodeArray[currentNodeIndex];
+
+         // reached end
+         if (currentNode.index == endNodeIndex)
+            break;
+
+         // remove current node from list
+         openList.RemoveAt(openList.FindIndex(_ => _ == currentNodeIndex));
+
+         closedList.Add(currentNodeIndex);
+
+         // check neighbours
+         for (int i = 0; i < neighbourOffsetsArray.Length; i++)
+         {
+            int2 neighbourPos = new int2(currentNode.x + neighbourOffsetsArray[i].x, currentNode.y + neighbourOffsetsArray[i].y);
+            int neighbourIndex = CalculateIndex(neighbourPos.x, neighbourPos.y);
+            PathNode neighbourNode = pathNodeArray[neighbourIndex];
+
+            // check if node is valid
+            if (isPositionInsideGrid(neighbourPos) == false ||
+                closedList.Contains(neighbourIndex) ||
+                pathNodeArray[neighbourIndex].isWalkable == false)
+               continue;
+
+            // change neighbour node values
+            int currentGCost = currentNode.gCost + CalculateDistanceCost(currentNode.Position(), neighbourPos);
+            if (currentGCost < neighbourNode.gCost)
+            {
+               neighbourNode.cameFromNodeIndex = currentNodeIndex;
+               neighbourNode.gCost = currentGCost;
+               neighbourNode.CalculateFCost();
+               pathNodeArray[neighbourIndex] = neighbourNode;
+
+               if (openList.Contains(neighbourIndex) == false)
+                  openList.Add(neighbourIndex);
+            }
+         }
+      }
+
+      if (endNode.cameFromNodeIndex != -1)
+      {
+         List<int2> path = CalculatePath();
+         path.Reverse();
+      }
+
+      void InitializeArray()
+      {
+         for (int x = 0; x < Constants.GRID_SIZE.x; x++)
+            for (int y = 0; y < Constants.GRID_SIZE.y; y++)
+            {
+               PathNode pathNode = new PathNode();
+               pathNode.x = x;
+               pathNode.y = y;
+               pathNode.index = CalculateIndex(x, y);
+
+               // TODO: gCost looks strange for me
+               pathNode.gCost = int.MaxValue; // we will change this so value doesn't really matter
+               pathNode.hCost = CalculateDistanceCost(new int2(x, y), endPosition);
+               pathNode.CalculateFCost();
+
+               pathNode.isWalkable = true;
+               pathNode.cameFromNodeIndex = -1; // we'll use -1 as invalid value
+
+               pathNodeArray[pathNode.index] = pathNode;
+            }
+      }
+
+      int CalculateIndex(int x, int y)
+      {
+         return x + y * Constants.GRID_SIZE.x;
       }
 
       int CalculateDistanceCost(int2 aPos, int2 bPos)
@@ -74,6 +150,43 @@ public class Pathfinding : MonoBehaviour
          int remaining = math.abs(xDistance - yDistance);
 
          return Constants.MOVE_DIAGONAL_COST * math.min(xDistance, yDistance) + Constants.MOVE_STRAIGHT_COST * remaining;
+      }
+
+      int GetLowestFCostNodeIndex(List<int> openList, PathNode[] pathNodeArray)
+      {
+         PathNode lowestCostNode = pathNodeArray[openList[0]];
+
+         for (int i = 1; i < openList.Count; i++)
+         {
+            PathNode currentNode = pathNodeArray[openList[i]];
+
+            if (currentNode.fCost < lowestCostNode.fCost)
+               lowestCostNode = currentNode;
+         }
+
+         return lowestCostNode.index;
+      }
+
+      bool isPositionInsideGrid(int2 gridPosition)
+      {
+         return gridPosition.x >= 0 && gridPosition.x < Constants.GRID_SIZE.x &&
+                gridPosition.y >= 0 && gridPosition.y < Constants.GRID_SIZE.y;
+      }
+
+      List<int2> CalculatePath()
+      {
+         var path = new List<int2>();
+         path.Add(new int2 (endNode.x,endNode.y));
+
+         PathNode currentNode = endNode;
+         while (currentNode.cameFromNodeIndex != -1)
+         {
+            PathNode cameFromNode = pathNodeArray[currentNode.cameFromNodeIndex];
+            path.Add(cameFromNode.index);
+            currentNode = cameFromNode;
+         }
+
+         return path;
       }
    }
 }
